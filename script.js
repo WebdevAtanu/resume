6d0a8744cbf5eee51cb6becce2d0d4405c874bd1
Atanu <atanum991@gmail.com>
2024-02-16 18:20:03 +0530

    mod

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-var typed = new Typed('#element', {
-    strings: ['HTML.', 'CSS.', 'JavaScript.', 'Node JS.', 'PHP.', 'Mongo DB.', 'MySQL.', 'Wordpress.'],
-    typeSpeed: 80,
-    loop: true
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
