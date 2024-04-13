8a2d48d2dfc2ffa6834f5bd4d508aaf9ae5e3b01
Atanu <atanum991@gmail.com>
2024-04-13 10:59:38 +0530

    Update

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel();
+});
+
 function mode() {
     let docBody = document.body;
     docBody.classList.toggle("dark_mode");
-}
+};
+
