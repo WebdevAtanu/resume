2c2dabf7f6defc9732e8977ca15f6f622207f559
Atanu <atanum991@gmail.com>
2024-04-16 16:51:56 +0530

    update

---
+++ old/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        // items:2,
+        loop: true,
+        // margin: 10,
+        autoplay: true,
+        // dots: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1000: {
+                items: 5,
+                nav: false,
+            }
+        }
+    });
+
+});
+
+function mode() {
+    let docBody = document.body;
+    docBody.classList.toggle("dark_mode");
+};
